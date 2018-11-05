90aa7b78605562a9925cbc2e2f8d0ff2c59d3067
rajeshmv <rajesh.mv@moveinsync.com>
2018-11-05 11:26:25 +0530

    changed PORT number

--- server.js
+++ server.js
@@ -74,8 +74,8 @@ app.post('/imageurl', (req,res) => { image.handleApiCall(req,res) });
 // // Load hash from your password DB.
 
 
-app.listen(3000,() => {
-   console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000,() => {
+   console.log(`app is running on port ${process.env.PORT}`);
 });
 
 /*
